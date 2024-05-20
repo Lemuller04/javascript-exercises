599d850c947f3d6781bafe6a8ded1e1866233935
Lemuller04 <troublessleepy@gmail.com>
2024-05-20 18:40:32 -0300

    Solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    if (timesToRepeat < 0) {
+        return "ERROR";
+    }
 
+    let stringRepeated = "";
+    for (let i = 0; i < timesToRepeat; i++) {
+        stringRepeated += stringToRepeat;
+    }
+    return stringRepeated;
 };
 
 // Do not edit below this line
