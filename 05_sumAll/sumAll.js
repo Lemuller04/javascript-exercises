84b7ec87fe5bad5c07fa232b608da292251ea597
Lemuller04 <troublessleepy@gmail.com>
2024-05-20 19:41:54 -0300

    Solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || typeof(x) != 'number' || typeof(y) != 'number') {
+        return "ERROR";
+    }
 
+    let result = 0;
+    let add = (x < y) ? 1 : -1;
+
+    while (x !== y) {
+        result += x;
+        x += add;
+    }
+
+    return result += x;
 };
 
 // Do not edit below this line
