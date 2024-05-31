6eda23a1ca61a536ef8066a0d819c0694333d44c
Lemuller04 <troublessleepy@gmail.com>
2024-05-31 10:18:40 -0300

    Solved 12/...

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(objIn) {
+    const sortedPeople = objIn.sort((a, b) => {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1
+    });
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
