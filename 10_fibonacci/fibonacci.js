212b26e7e17fa52d312dbceb6de37aae06020bfc
Lemuller04 <troublessleepy@gmail.com>
2024-05-31 09:39:40 -0300

    Solved 10/...

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    let numInt = parseInt(num);
+
+    if (numInt == 1 || numInt == 2) {
+        return 1;
+    }
+
+    let numbers = [1,1];
+    let sum = 0;
+    let i = 0;
+
+    while (i < numInt-2) {
+        sum = numbers[i] + numbers[i+1];
+        numbers.push(sum);
+        i++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
