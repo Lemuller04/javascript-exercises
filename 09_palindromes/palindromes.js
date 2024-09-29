e239cd6655d909f598a58068f99153973a0fa98a
Lemuller04 <leonardomullerdelima@pm.me>
2024-09-28 22:16:41 -0300

    update 09_palindromes resolution
    Removed the use of some variables to a more succinct code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,28 +1,22 @@
 const palindromes = function (str) {
     let formattedStr = "";
 
-    // PERFORMS A CHECK ON EACH CHARACTER OF THE STRING
+    // INCLUDES ONLY VALID ALPHANUMERIC CHARACTERS
     for (let i = 0; i < str.length; i++) {
         if (isValid(str[i])) {
             formattedStr += (str[i]).toLowerCase();
         }
     }
 
-    let reversed = [...formattedStr].reverse().join(''); // REVERSES THE STRING
-    let halfStr = formattedStr.slice(formattedStr.length/2); // GETS LAST HALF OF THE STRING
-    let halfReversed = reversed.slice(reversed.length/2) // GETS LAST HALF OF THE REVERSED STRING
-
-    // COMPARES EACH CHARACTER FROM LAST HALVES OF STRING AND REVERSED STRING
-    for (let i = 0; i < halfStr.length; i++) {
-        if (!(halfStr[i] === halfReversed[i])) {
+    for (let i = 0, l = formattedStr.length; i < l; i++) {
+        if (formattedStr[i] != formattedStr[l - 1 - i]) {
             return false;
         }
     }
 
     // CHECKS IF A CHARACTER IS A VALID NUMBER OR LETTER IN THE ASCII TABLE
-    function isValid(str) {
-        c = str.charCodeAt();
-        return (c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58);
+    function isValid(c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
     }
 
     return true;
